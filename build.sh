3e0ded93a853175c2081523d6476827a16197b77
Rick Arts <rick@roefja.com>
2021-03-04 13:51:32 +0100

    Updated build script to only create the public image

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@
 BASENAME='wordpress'
 PROJECT='library'
 
-# We are publishing via our own registry. Our own registry replicates to the public Docker Hub Repo
-docker build -q -t registry.roefja.dev/"$PROJECT"/"$BASENAME" .
-docker push -q registry.roefja.dev/library/"$BASENAME" 
+docker build -q -t roefja/wordpress .
